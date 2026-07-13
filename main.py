9418f114271c0b0239f53f886912635f33a6bacc
yeowon08 <2kyw1015@gmail.com>
2026-07-13 10:42:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ st.title("나의 첫 웹앱")
 st.write("안녕하세요! 반갑습니다.")
 지역 = st.selectbox("지역을 골라 보세요", ["서울", "양평", "부산"])
 st.write("당신이 고른 지역:", 지역)
+숫자 = st.slider("좋아하는 숫자", 0, 100)
+st.write("고른 숫자:", 숫자)
+ 
+if st.button("풍선 날리기"):
+	st.balloons()
 
